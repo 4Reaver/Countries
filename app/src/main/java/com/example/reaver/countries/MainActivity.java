2e7b08d7c15118bad30233f1efb88114c46064fe
Reaver <ellis.neo.reg@gmail.com>
2014-07-16 19:40:11 +0300

    Create list of countries

--- app/src/main/java/com/example/reaver/countries/MainActivity.java
+++ app/src/main/java/com/example/reaver/countries/MainActivity.java
@@ -2,16 +2,47 @@ package com.example.reaver.countries;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
+    private ArrayList<String> countries = new ArrayList<String>();
+    private ArrayList<String> squares = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter;
+        ListView lvMain;
+        String name;
+        String square;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            InputStream is = getAssets().open("namesandsquares.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            while ( (name = br.readLine()) != null && (square = br.readLine()) != null ) {
+                countries.add(name);
+                squares.add(square);
+            }
+        } catch (IOException e) {
+            Log.d("MY_LOG", "File \"namesandsquares\" not found");
+        }
+
+        lvMain = (ListView) findViewById(R.id.listViewMain);
+        adapter = new ArrayAdapter<String>(this, R.layout.item, countries);
+        lvMain.setAdapter(adapter);
     }
 
 
