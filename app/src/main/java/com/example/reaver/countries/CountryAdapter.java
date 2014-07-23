d1b0ca3d3188c49420dc4d6f243797ad1e4e985b
Reaver <ellis.neo.reg@gmail.com>
2014-07-23 16:00:44 +0300

    Add customAdapter and change filter

---
+++ app/src/main/java/com/example/reaver/countries/CountryAdapter.java
@@ -0,0 +1,104 @@
+package com.example.reaver.countries;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Reaver on 23.07.2014.
+ */
+public class CountryAdapter extends BaseAdapter implements Filterable {
+    private Context context;
+    private LayoutInflater lInflater;
+    private ArrayList<Country> countries;
+    private ArrayList<Country> originalCountries;
+
+    public CountryAdapter(Context context, ArrayList<Country> countries) {
+        this.context = context;
+        this.countries = countries;
+        lInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public ArrayList<Country> getCountries() {
+        return countries;
+    }
+
+    @Override
+    public int getCount() {
+        return countries.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return countries.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        Country c = (Country) getItem(position);
+
+        if ( view == null ) {
+            view = lInflater.inflate(R.layout.item, parent, false);
+        }
+
+        ((ImageView) view.findViewById(R.id.flag)).setImageResource(R.drawable.ic_launcher);
+        ((TextView) view.findViewById(R.id.country_name)).setText(c.getName());
+
+        return view;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                ArrayList<Country> filteredResult = getFilteredResults(constraint);
+                FilterResults results = new FilterResults();
+
+                results.values = filteredResult;
+                return results;
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                countries = (ArrayList<Country>) filterResults.values;
+                CountryAdapter.this.notifyDataSetChanged();
+            }
+
+            private ArrayList<Country> getFilteredResults(CharSequence constraint) {
+                ArrayList<Country> result = new ArrayList<Country>();
+                String cons = constraint.toString().toLowerCase();
+
+                if ( originalCountries == null ) {
+                    originalCountries = new ArrayList<Country>(countries);
+                }
+
+                if ( constraint != null && constraint.length() > 0) {
+                    for ( Country c : originalCountries ) {
+                        if ( c.getName().toLowerCase().contains(cons) ) {
+                            result.add(c);
+                        }
+                    }
+
+                    return result;
+                }
+                return new ArrayList<Country>(originalCountries);
+            }
+        };
+    }
+}
--- app/src/main/java/com/example/reaver/countries/MainActivity.java
+++ app/src/main/java/com/example/reaver/countries/MainActivity.java
@@ -31,10 +31,9 @@ public class MainActivity extends Activity implements View.OnClickListener, Frag
         AdapterView.OnItemClickListener, TextWatcher {
     private static final int DELETE_ID = 1;
 
-    private ArrayAdapter<Country> adapter;
-    //private ArrayList<String> countries = new ArrayList<String>();
+    //private ArrayAdapter<Country> adapter;
+    private CountryAdapter adapter;
     private ArrayList<Country> countries = new ArrayList<Country>();
-    //private ArrayList<String> squares = new ArrayList<String>();
     private DialogFragment fragmentAdd;
     private EditText filter;
 
@@ -46,34 +45,16 @@ public class MainActivity extends Activity implements View.OnClickListener, Frag
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            InputStream is = getAssets().open("names_s_a.txt");
-            Scanner scanner = new Scanner(is).useLocale(Locale.US);
-            String name;
-            int area;
-            int population;
-
-
-            while ( scanner.hasNextLine() ) {
-                name = scanner.nextLine();
-                area = scanner.nextInt();
-                population = scanner.nextInt();
-                countries.add(new Country(name, area, population));
-                scanner.nextLine();
-            }
-        } catch (IOException e) {
-            Log.d("MY_LOG", "File \"namesandsquares\" not found");
-        }
+        fillData();
 
         addButton = (Button) findViewById(R.id.addButton);
         addButton.setOnClickListener(this);
         filter = (EditText) findViewById(R.id.filter);
         filter.addTextChangedListener(this);
         lvMain = (ListView) findViewById(R.id.listViewMain);
-        adapter = new ArrayAdapter<Country>(this, R.layout.item, countries);
+        adapter = new CountryAdapter(this, countries);
         lvMain.setAdapter(adapter);
 
-        //lvMain.setOnItemLongClickListener(this);
         lvMain.setOnItemClickListener(this);
         registerForContextMenu(lvMain);
         fragmentAdd = new FragmentAdd();
@@ -98,6 +79,27 @@ public class MainActivity extends Activity implements View.OnClickListener, Frag
         return super.onOptionsItemSelected(item);
     }
 
+    private void fillData() {
+        try {
+            InputStream is = getAssets().open("names_s_a.txt");
+            Scanner scanner = new Scanner(is).useLocale(Locale.US);
+            String name;
+            int area;
+            int population;
+
+
+            while ( scanner.hasNextLine() ) {
+                name = scanner.nextLine();
+                area = scanner.nextInt();
+                population = scanner.nextInt();
+                countries.add(new Country(name, area, population));
+                scanner.nextLine();
+            }
+        } catch (IOException e) {
+            Log.d("MY_LOG", "File \"namesandsquares\" not found");
+        }
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
@@ -110,6 +112,7 @@ public class MainActivity extends Activity implements View.OnClickListener, Frag
     @Override
     public void onOkButtonClicked(Country newCountry) {
         Toast.makeText(this, "Добавлена страна: ".concat(newCountry.toString()), Toast.LENGTH_SHORT).show();
+        countries = adapter.getCountries();
         countries.add(newCountry);
         adapter.notifyDataSetChanged();
     }
@@ -125,6 +128,7 @@ public class MainActivity extends Activity implements View.OnClickListener, Frag
         if ( item.getItemId() == DELETE_ID ) {
             AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
 
+            countries = adapter.getCountries();
             countries.remove(info.position);
             adapter.notifyDataSetChanged();
         }
@@ -141,7 +145,7 @@ public class MainActivity extends Activity implements View.OnClickListener, Frag
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra("Country", countries.get(i));
+        intent.putExtra("Country", adapter.getCountries().get(i));
         startActivity(intent);
     }
 
@@ -153,7 +157,6 @@ public class MainActivity extends Activity implements View.OnClickListener, Frag
     @Override
     public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
         adapter.getFilter().filter(filter.getText().toString());
-        adapter.notifyDataSetChanged();
     }
 
     @Override
